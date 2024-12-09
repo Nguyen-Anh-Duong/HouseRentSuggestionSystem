3a1499f5ba5a16a440e8a422f20433cd77d6a412
naDuong2004 <nguyenanhduong112004@gmail.com>
2024-12-10 06:58:38 +0700

    t

--- routes/suggestion.js
+++ routes/suggestion.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/suggestion", function (req, res, next) {
+  const { price, area, max_people, address, feature } = req.body;
+
   // goi ham query
   res.render("index", { title: "Hello" });
 });
